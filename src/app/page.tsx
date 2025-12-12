d3774ff9d067ff93898cd061ebcb9ef2dbd3496f
Jason <KryptikOne@users.noreply.github.com>
2025-12-12 12:03:30 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Header from '@/app/ui/global/header'
+import Header from "@/app/ui/global/header";
 
 export default function Home() {
-
-  return (
-    <div className="page min-h-full">
-      <Header />
-      <main className="bg-gray-300 w-full">
-        <div className="mx-auto max-w-7xl h-full px-4 py-6 sm:px-6 lg:px-8">
-          <p className="text-gray-900">Main Section</p>
-        </div>
-      </main>
-    </div>
-  )
+	return (
+		<div className="page min-h-full">
+			<Header />
+			<main className="bg-gray-300 w-full">
+				<div className="mx-auto max-w-7xl h-full px-4 py-6 sm:px-6 lg:px-8">
+					<p className="text-gray-900">Main Section 👋🏽</p>
+				</div>
+			</main>
+		</div>
+	);
 }
