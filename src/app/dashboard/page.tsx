caeaf2f6fc13f149d8324a3a11654564fba9c954
Jason <KryptikOne@users.noreply.github.com>
2025-12-12 12:12:37 -0600

    Lets try again

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Header from '@/app/ui/global/header'
-import DashboardClient from '@/app/ui/dashboard/DashboardClient'
-import { fetchCurrentWeather } from '../lib/utils'
+import React from "react";
+import Header from "@/app/ui/global/header";
+// import DashboardClient from '@/app/ui/dashboard/DashboardClient'
+// import { fetchCurrentWeather } from '../lib/utils'
 
 export default async function DashboardPage() {
-  const currentWeatherData = await fetchCurrentWeather()
+	// const currentWeatherData = await fetchCurrentWeather()
 
-  return (
-    <div className="page dashboard min-h-full">
-      <Header />
-      <DashboardClient currentdata={currentWeatherData} />
-    </div>
-  )
+	return (
+		<div className="page dashboard min-h-full">
+			<Header />
+			{/* <DashboardClient currentdata={currentWeatherData} /> */}
+		</div>
+	);
 }
